c389c4fa1462a1c6f250e23ab6bf1967e5c9c69c
AloisGoeury <alois.goeury@mines-albi.fr>
2023-10-25 00:30:03 +0200

    Mettre les tables dans l'ordre pour les créer dans postgresql

--- Sprint 1/Phase 3/bdd.sql
+++ Sprint 1/Phase 3/bdd.sql
@@ -22,6 +22,211 @@ drop table if exists type_etablissement cascade;
 drop table if exists type_note cascade;
 drop table if exists "user" cascade;
 
+-- -----------------------------------------------------
+-- Table user
+-- -----------------------------------------------------
+
+create table "user" (
+  "id_user" serial primary key,
+  "username" varchar(100) not null,
+  "role" varchar(45) not null,
+  "nom" varchar(45),
+  "prenom" varchar(45),
+  "date_de_naissance" date not null,
+  "genre" varchar(45),
+  "email" varchar(70) not null,
+  "adresse" varchar(45),
+  "password" varchar(45),
+  "ppbin" bytea,
+  "ppform" text,
+  "langue" varchar(45) not null,
+  "description" text,
+  constraint avatar check (
+    (ppbin is null and ppform is null)
+    or
+    (ppbin is not null and ppform is not null)
+  )
+);
+
+
+-- CREATE TABLE IF NOT EXISTS `mydb`.`User` (
+--   `idUser` INT NOT NULL AUTO_INCREMENT,
+--   `username` VARCHAR(100) NOT NULL,
+--   `role` VARCHAR(45) NOT NULL,
+--   `nom` VARCHAR(45) NULL,
+--   `prenom` VARCHAR(45) NULL,
+--   `dateDeNaissance` DATE NOT NULL,
+--   `genre` VARCHAR(45) NULL,
+--   `email` VARCHAR(45) NOT NULL,
+--   `adresse` VARCHAR(45) NULL,
+--   `password` VARCHAR(45) NULL,
+--   `ppbin` MEDIUMBLOB NULL,
+--   `ppform` VARCHAR(45) NULL,
+--   `langue` VARCHAR(45) NOT NULL,
+--   `description` LONGTEXT NULL,
+--   PRIMARY KEY (`idUser`),
+--   UNIQUE INDEX `idUser_UNIQUE` (`idUser` ASC) VISIBLE)
+-- ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table etablissement
+-- -----------------------------------------------------
+
+create table "etablissement" (
+  "id_etablissement" serial primary key,
+  "adresse" text not null,
+  "nom" varchar(45) not null,
+  "approuved" boolean default false,
+  "description" text
+);
+
+
+
+-- CREATE TABLE IF NOT EXISTS `mydb`.`Etablissement` (
+--   `idEtablissement` INT NOT NULL AUTO_INCREMENT,
+--   `adresse` VARCHAR(45) NOT NULL,
+--   `nom` VARCHAR(45) NOT NULL,
+--   `approuved` TINYINT NULL DEFAULT 0,
+--   `description` LONGTEXT NULL,
+--   PRIMARY KEY (`idEtablissement`))
+-- ENGINE = InnoDB;
+
+-- -----------------------------------------------------
+-- Table note
+-- -----------------------------------------------------
+
+create table "note" (
+  "id_note" serial,
+  "id_etablissement" int not null,
+  "id_user" int not null,
+  -- "date" date null,
+  primary key ("id_note", "id_etablissement", "id_user"),
+  constraint "etablissement"
+    foreign key ("id_etablissement")
+    references "etablissement" ("id_etablissement")
+    on delete cascade
+    on update cascade,
+  constraint "user"
+    foreign key ("id_user")
+    references "user" ("id_user")
+    on delete cascade
+    on update cascade
+);
+
+create unique index idnote_unique on "note" ("id_note");
+create index etablissement_note_idx on "note" ("id_etablissement");
+create index user_note_idx on "note" ("id_user");
+
+
+-- CREATE TABLE IF NOT EXISTS `mydb`.`Note` (
+--   `idNote` INT NOT NULL AUTO_INCREMENT,
+--   `idEtablissement` INT NOT NULL,
+--   `idUser` INT NOT NULL,
+--   `date` DATETIME NULL,
+--   PRIMARY KEY (`idNote`, `idEtablissement`, `idUser`),
+--   UNIQUE INDEX `idNote_UNIQUE` (`idNote` ASC) VISIBLE,
+--   INDEX `etablissement_idx` (`idEtablissement` ASC) VISIBLE,
+--   INDEX `user_idx` (`idUser` ASC) VISIBLE,
+--   CONSTRAINT `etablissement`
+--     FOREIGN KEY (`idEtablissement`)
+--     REFERENCES `mydb`.`Etablissement` (`idEtablissement`)
+--     ON DELETE cascade
+--     ON UPDATE cascade,
+--   CONSTRAINT `user`
+--     FOREIGN KEY (`idUser`)
+--     REFERENCES `mydb`.`User` (`idUser`)
+--     ON DELETE cascade
+--     ON UPDATE cascade)
+-- ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table post
+-- -----------------------------------------------------
+
+create table "post" (
+  "id_post" serial primary key,
+  "id_user" int not null,
+  "text" text,
+  "date" date not null,
+  "type" varchar(45) not null,
+  "afficher?" boolean not null default true,
+  "id_note" int,
+  constraint "id_user"
+    foreign key ("id_user")
+    references "user" ("id_user")
+    on delete cascade
+    on update cascade,
+  constraint "id_note"
+    foreign key ("id_note")
+    references "note" ("id_note")
+    on delete cascade
+    on update cascade
+);
+
+create unique index idpost_post_unique on "post" ("id_post");
+create index iduser_post_idx on "post" ("id_user");
+create index idnote_post_idx on "post" ("id_note");
+
+
+-- CREATE TABLE IF NOT EXISTS `mydb`.`Post` (
+--   `idPost` INT NOT NULL AUTO_INCREMENT,
+--   `idUser` INT NOT NULL,
+--   `text` LONGTEXT NULL,
+--   `date` DATETIME NOT NULL,
+--   `type` VARCHAR(45) NOT NULL,
+--   `afficher?` TINYINT NOT NULL DEFAULT 1,
+--   `idNote` INT NULL,
+--   PRIMARY KEY (`idPost`),
+--   UNIQUE INDEX `idPost_UNIQUE` (`idPost` ASC) VISIBLE,
+--   INDEX `idUser_idx` (`idUser` ASC) VISIBLE,
+--   INDEX `idnote_idx` (`idNote` ASC) VISIBLE,
+--   CONSTRAINT `idUser`
+--     FOREIGN KEY (`idUser`)
+--     REFERENCES `mydb`.`User` (`idUser`)
+--     ON DELETE cascade
+--     ON UPDATE cascade,
+--   CONSTRAINT `idnote`
+--     FOREIGN KEY (`idNote`)
+--     REFERENCES `mydb`.`Note` (`idNote`)
+--     ON DELETE cascade
+--     ON UPDATE cascade)
+-- ENGINE = InnoDB;
+
+-- -----------------------------------------------------
+-- Table type_etablissement
+-- -----------------------------------------------------
+
+create table "type_etablissement" (
+  "id_type_etablissement" serial primary key,
+  "nom" varchar(45)
+);
+
+
+-- CREATE TABLE IF NOT EXISTS `mydb`.`TypeEtablissement` (
+--   `idTypeEtablissement` INT NOT NULL,
+--   `nom` VARCHAR(45) NULL,
+--   PRIMARY KEY (`idTypeEtablissement`))
+-- ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table type_note
+-- -----------------------------------------------------
+
+create table "type_note" (
+  "id_type_note" serial primary key,
+  "nom" varchar(45) not null
+);
+
+
+-- CREATE TABLE IF NOT EXISTS `mydb`.`TypeNote` (
+--   `idTypeNote` INT NOT NULL AUTO_INCREMENT,
+--   `nom` VARCHAR(45) NOT NULL,
+--   PRIMARY KEY (`idTypeNote`))
+-- ENGINE = InnoDB;
+
 
 -- -----------------------------------------------------
 -- Table abonnement
@@ -30,20 +235,19 @@ drop table if exists "user" cascade;
 create table abonnement (
   id_user1 int not null,
   id_user2 int not null,
-  primary key (idUser1, idUser2),
+  primary key (id_user1, id_user2),
   constraint user_fk
-    foreign key (idUser1)
+    foreign key (id_user1)
     references "user" (id_user)
-    on delete cascade
     on delete cascade,
   constraint user2_fk
-    foreign key (idUser2)
+    foreign key (id_user2)
     references "user" (id_user)
-    on delete cascade
     on delete cascade
 );
 
-create index user2_abonenement_idx on "abonnement" ("iduser2");
+create index user2_abonnement_idx on abonnement (id_user2);
+
 
 
 
@@ -67,51 +271,6 @@ create index idcollection_idx on "collection" ("id_collection");
 --   PRIMARY KEY (`idCollection`),
 --   UNIQUE INDEX `idCollection_UNIQUE` (`idCollection` ASC) VISIBLE)
 -- ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table contenu_collection
--- -----------------------------------------------------
-
-create table contenu_collection (
-  "id_user" int not null,
-  "id_post" int not null,
-  "id_collection" int not null,
-  primary key ("id_user", "id_post", "id_collection"),
-  constraint enregistrement
-    foreign key ("id_user", "id_post")
-    references "enregistrement_post" ("id_user", "id_post")
-    on delete cascade
-    on delete cascade,
-  constraint "collection"
-    foreign key ("id_collection")
-    references "collection" ("id_collection")
-    on delete cascade
-    on delete cascade
-);
-
-create index contenu_collection_idx on "contenu_collection" ("id_collection");
-
-
-
--- CREATE TABLE IF NOT EXISTS `mydb`.`ContenuCollection` (
---   `idUser` INT NOT NULL,
---   `idPost` INT NOT NULL,
---   `idCollection` INT NOT NULL,
---   PRIMARY KEY (`idUser`, `idPost`, `idCollection`),
---   INDEX `collection_idx` (`idCollection` ASC) VISIBLE,
---   CONSTRAINT `enregistrement`
---     FOREIGN KEY (`idUser` , `idPost`)
---     REFERENCES `mydb`.`EnregistrementPost` (`idUser` , `idPost`)
---     ON DELETE cascade
---     ON UPDATE cascade,
---   CONSTRAINT `collection`
---     FOREIGN KEY (`idCollection`)
---     REFERENCES `mydb`.`Collection` (`idCollection`)
---     ON DELETE cascade
---     ON UPDATE cascade)
--- ENGINE = InnoDB;
-
 
 -- -----------------------------------------------------
 -- Table enregistrement_post
@@ -152,6 +311,50 @@ create index user_enregistrement_post_idx on "enregistrement_post" ("id_user");
 --     ON DELETE cascade
 --     ON UPDATE cascade)
 -- ENGINE = InnoDB;
+
+-- -----------------------------------------------------
+-- Table contenu_collection
+-- -----------------------------------------------------
+
+create table contenu_collection (
+  "id_user" int not null,
+  "id_post" int not null,
+  "id_collection" int not null,
+  primary key ("id_user", "id_post", "id_collection"),
+  constraint "enregistrement"
+    foreign key ("id_user", "id_post")
+    references "enregistrement_post" ("id_user", "id_post")
+    on delete cascade,
+  constraint "collection"
+    foreign key ("id_collection")
+    references "collection" ("id_collection")
+    on delete cascade
+);
+
+create index contenu_collection_idx on "contenu_collection" ("id_collection");
+
+
+
+-- CREATE TABLE IF NOT EXISTS `mydb`.`ContenuCollection` (
+--   `idUser` INT NOT NULL,
+--   `idPost` INT NOT NULL,
+--   `idCollection` INT NOT NULL,
+--   PRIMARY KEY (`idUser`, `idPost`, `idCollection`),
+--   INDEX `collection_idx` (`idCollection` ASC) VISIBLE,
+--   CONSTRAINT `enregistrement`
+--     FOREIGN KEY (`idUser` , `idPost`)
+--     REFERENCES `mydb`.`EnregistrementPost` (`idUser` , `idPost`)
+--     ON DELETE cascade
+--     ON UPDATE cascade,
+--   CONSTRAINT `collection`
+--     FOREIGN KEY (`idCollection`)
+--     REFERENCES `mydb`.`Collection` (`idCollection`)
+--     ON DELETE cascade
+--     ON UPDATE cascade)
+-- ENGINE = InnoDB;
+
+
+
 
 
 -- -----------------------------------------------------
@@ -195,28 +398,7 @@ create index post2_commentairede_idx on "est_commentaire_de" ("id_post2");
 -- ENGINE = InnoDB;
 
 
--- -----------------------------------------------------
--- Table etablissement
--- -----------------------------------------------------
 
-create table "etablissement" (
-  "id_etablissement" serial primary key,
-  "adresse" text not null,
-  "nom" varchar(45) not null,
-  "approuved" boolean default false,
-  "description" text
-);
-
-
-
--- CREATE TABLE IF NOT EXISTS `mydb`.`Etablissement` (
---   `idEtablissement` INT NOT NULL AUTO_INCREMENT,
---   `adresse` VARCHAR(45) NOT NULL,
---   `nom` VARCHAR(45) NOT NULL,
---   `approuved` TINYINT NULL DEFAULT 0,
---   `description` LONGTEXT NULL,
---   PRIMARY KEY (`idEtablissement`))
--- ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
@@ -403,53 +585,7 @@ create index post_motcle_idx on "mot_cle_post" ("id_post");
 -- ENGINE = InnoDB;
 
 
--- -----------------------------------------------------
--- Table note
--- -----------------------------------------------------
 
-create table "note" (
-  "id_note" serial,
-  "id_etablissement" int not null,
-  "id_user" int not null,
-  -- "date" date null,
-  primary key ("id_note", "id_etablissement", "id_user"),
-  constraint "etablissement"
-    foreign key ("id_etablissement")
-    references "etablissement" ("id_etablissement")
-    on delete cascade
-    on update cascade,
-  constraint "user"
-    foreign key ("id_user")
-    references "user" ("id_user")
-    on delete cascade
-    on update cascade
-);
-
-create unique index idnote_unique on "note" ("id_note");
-create index etablissement_note_idx on "note" ("id_etablissement");
-create index user_note_idx on "note" ("id_user");
-
-
--- CREATE TABLE IF NOT EXISTS `mydb`.`Note` (
---   `idNote` INT NOT NULL AUTO_INCREMENT,
---   `idEtablissement` INT NOT NULL,
---   `idUser` INT NOT NULL,
---   `date` DATETIME NULL,
---   PRIMARY KEY (`idNote`, `idEtablissement`, `idUser`),
---   UNIQUE INDEX `idNote_UNIQUE` (`idNote` ASC) VISIBLE,
---   INDEX `etablissement_idx` (`idEtablissement` ASC) VISIBLE,
---   INDEX `user_idx` (`idUser` ASC) VISIBLE,
---   CONSTRAINT `etablissement`
---     FOREIGN KEY (`idEtablissement`)
---     REFERENCES `mydb`.`Etablissement` (`idEtablissement`)
---     ON DELETE cascade
---     ON UPDATE cascade,
---   CONSTRAINT `user`
---     FOREIGN KEY (`idUser`)
---     REFERENCES `mydb`.`User` (`idUser`)
---     ON DELETE cascade
---     ON UPDATE cascade)
--- ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
@@ -495,58 +631,6 @@ create index type_note_idx on "note_concerne" ("id_type_note");
 -- ENGINE = InnoDB;
 
 
--- -----------------------------------------------------
--- Table post
--- -----------------------------------------------------
-
-create table "post" (
-  "id_post" serial primary key,
-  "id_user" int not null,
-  "text" text,
-  "date" date not null,
-  "type" varchar(45) not null,
-  "afficher?" boolean not null default true,
-  "id_note" int,
-  constraint "id_user"
-    foreign key ("id_user")
-    references "user" ("id_user")
-    on delete cascade
-    on update cascade,
-  constraint "id_note"
-    foreign key ("id_note")
-    references "note" ("id_note")
-    on delete cascade
-    on update cascade
-);
-
-create unique index idpost_post_unique on "post" ("id_post");
-create index iduser_post_idx on "post" ("id_user");
-create index idnote_post_idx on "post" ("id_note");
-
-
--- CREATE TABLE IF NOT EXISTS `mydb`.`Post` (
---   `idPost` INT NOT NULL AUTO_INCREMENT,
---   `idUser` INT NOT NULL,
---   `text` LONGTEXT NULL,
---   `date` DATETIME NOT NULL,
---   `type` VARCHAR(45) NOT NULL,
---   `afficher?` TINYINT NOT NULL DEFAULT 1,
---   `idNote` INT NULL,
---   PRIMARY KEY (`idPost`),
---   UNIQUE INDEX `idPost_UNIQUE` (`idPost` ASC) VISIBLE,
---   INDEX `idUser_idx` (`idUser` ASC) VISIBLE,
---   INDEX `idnote_idx` (`idNote` ASC) VISIBLE,
---   CONSTRAINT `idUser`
---     FOREIGN KEY (`idUser`)
---     REFERENCES `mydb`.`User` (`idUser`)
---     ON DELETE cascade
---     ON UPDATE cascade,
---   CONSTRAINT `idnote`
---     FOREIGN KEY (`idNote`)
---     REFERENCES `mydb`.`Note` (`idNote`)
---     ON DELETE cascade
---     ON UPDATE cascade)
--- ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
@@ -648,83 +732,8 @@ create index regime_user_idx on "regimes_de_l_user" ("id_regime");
 -- ENGINE = InnoDB;
 
 
--- -----------------------------------------------------
--- Table type_etablissement
--- -----------------------------------------------------
-
-create table "type_etablissement" (
-  "id_type_etablissement" serial primary key,
-  "nom" varchar(45)
-);
 
 
--- CREATE TABLE IF NOT EXISTS `mydb`.`TypeEtablissement` (
---   `idTypeEtablissement` INT NOT NULL,
---   `nom` VARCHAR(45) NULL,
---   PRIMARY KEY (`idTypeEtablissement`))
--- ENGINE = InnoDB;
 
 
--- -----------------------------------------------------
--- Table type_note
--- -----------------------------------------------------
-
-create table "type_note" (
-  "id_type_note" serial primary key,
-  "nom" varchar(45) not null
-);
-
-
--- CREATE TABLE IF NOT EXISTS `mydb`.`TypeNote` (
---   `idTypeNote` INT NOT NULL AUTO_INCREMENT,
---   `nom` VARCHAR(45) NOT NULL,
---   PRIMARY KEY (`idTypeNote`))
--- ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table user
--- -----------------------------------------------------
-
-create table "user" (
-  "id_user" serial primary key,
-  "username" varchar(100) not null,
-  "role" varchar(45) not null,
-  "nom" varchar(45),
-  "prenom" varchar(45),
-  "date_de_naissance" date not null,
-  "genre" varchar(45),
-  "email" varchar(70) not null,
-  "adresse" varchar(45),
-  "password" varchar(45),
-  "ppbin" bytea,
-  "ppform" text,
-  "langue" varchar(45) not null,
-  "description" text,
-  constraint avatar check (
-    (ppbin is null and ppform is null)
-    or
-    (ppbin is not null and ppform is not null)
-  )
-);
-
-
--- CREATE TABLE IF NOT EXISTS `mydb`.`User` (
---   `idUser` INT NOT NULL AUTO_INCREMENT,
---   `username` VARCHAR(100) NOT NULL,
---   `role` VARCHAR(45) NOT NULL,
---   `nom` VARCHAR(45) NULL,
---   `prenom` VARCHAR(45) NULL,
---   `dateDeNaissance` DATE NOT NULL,
---   `genre` VARCHAR(45) NULL,
---   `email` VARCHAR(45) NOT NULL,
---   `adresse` VARCHAR(45) NULL,
---   `password` VARCHAR(45) NULL,
---   `ppbin` MEDIUMBLOB NULL,
---   `ppform` VARCHAR(45) NULL,
---   `langue` VARCHAR(45) NOT NULL,
---   `description` LONGTEXT NULL,
---   PRIMARY KEY (`idUser`),
---   UNIQUE INDEX `idUser_UNIQUE` (`idUser` ASC) VISIBLE)
--- ENGINE = InnoDB;
 
