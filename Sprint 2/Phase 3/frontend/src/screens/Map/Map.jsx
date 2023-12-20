03889ed764c59b30ab1fd6a8d45f0918008523cb
Aicha <aicha.chaaba@mines-albi.fr>
2023-12-20 17:18:27 +0100

    "add itinerary"

--- Sprint 2/Phase 3/frontend/src/screens/Map/Map.jsx
+++ Sprint 2/Phase 3/frontend/src/screens/Map/Map.jsx
@@ -7,9 +7,12 @@ import SearchBar from '../../components/SearchBar/searchbar';
 import { MapContainer, TileLayer } from 'react-leaflet'
 import { Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
-
+import L from "leaflet";
 import Skeleton from '@mui/material/Skeleton';
 import Stack from '@mui/material/Stack';
+import "leaflet-control-geocoder/dist/Control.Geocoder.css";
+import "leaflet-control-geocoder/dist/Control.Geocoder.js";
+import LeafletGeocoder from './LeafletGeocoder';
 
 function Map() {
   const navigate = useNavigate();
@@ -40,12 +43,13 @@ function Map() {
               <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-              />
+              /> {/*
               <Marker position={[43.92517082408836, 2.147408244346074]}>
                 <Popup>
                   Restaurant <br /> Description.
                 </Popup>
-              </Marker>
+              </Marker>*/}
+              <LeafletGeocoder></LeafletGeocoder>
             </MapContainer>
           </div>
         </div>
@@ -53,5 +57,13 @@ function Map() {
     </div>
   );
 }
+
+let DefaultIcon = L.icon({
+  iconUrl: "https://unpkg.com/leaflet@1.6/dist/images/marker-icon.png",
+  iconSize: [20, 36],
+  iconAnchor: [10, 41],
+  popupAnchor: [2, -40],
+});
+L.Marker.prototype.options.icon = DefaultIcon;
 
 export default Map;
